62d51cd9d3d4234746dcae793e9a891103ab8417
Chinmaya Jena <mail2chinmaya.jena@gmail.com>
2019-02-17 11:22:21 +0530

    redux added

---
+++ src/components/App.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const App = () => <div>"Hello User!"</div>;
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 3000
   }
 };
