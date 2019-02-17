78eed4a897f75f627c07b4c91b77b361e700bd45
Chinmaya Jena <mail2chinmaya.jena@gmail.com>
2019-02-17 12:02:56 +0530

    fix bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { logger } from "redux-logger";
@@ -8,7 +9,11 @@ import reducer from "./reducers";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import App from "./components/App";
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
 render(
   <Provider store={store}>
     <App />
