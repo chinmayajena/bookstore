555066469c6cc09a464f72318c71472c36074421
Chinmaya Jena <mail2chinmaya.jena@gmail.com>
2019-02-18 15:35:39 +0530

    fix initial test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,10 @@
 import React from "react";
-import renderer from "react-test-renderer";
-
+import { shallow } from 'enzyme';
+ 
 import App from "./App";
-
-it("renders correctly when there are no items", () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('<App />', () => {  
+    test('renders the component', () => {
+      const wrapper = shallow(<App />);
+       expect(wrapper).toMatchSnapshot();
+    }); 
 });
