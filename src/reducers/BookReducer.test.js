e50970b9d6024d9095bc4377e15bebc4fbd3d18e
Chinmaya Jena <mail2chinmaya.jena@gmail.com>
2019-02-19 08:27:19 +0530

    fix remove book reducer

--- src/reducers/BookReducer.test.js
+++ src/reducers/BookReducer.test.js
@@ -27,4 +27,26 @@ describe("BookReducer", () => {
       }
     ]);
   });
+
+  it("should handle REMOVE_BOOK", () => {
+    expect(
+      bookReducer(
+        [
+          {
+            id: 4,
+            name: "Ken n Abel",
+            author: "Jeffry Archer",
+            description: "A philosophical book regarding life journey",
+            category: "Philosophy",
+            content: "Some book content",
+            isBookmarked: false
+          }
+        ],
+        {
+          type: REMOVE_BOOK,
+          id: 4
+        }
+      )
+    ).toEqual([]);
+  });
 });
