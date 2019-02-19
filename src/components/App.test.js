bd48c3aea5291a25a3f3959a09ea46374e3f2ca8
Chinmaya Jena <mail2chinmaya.jena@gmail.com>
2019-02-19 12:57:10 +0530

    snapshot added

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,6 +5,7 @@ import App from "./App";
 describe("<App />", () => {
   it("renders the component", () => {
     const component = shallow(<App />);
+    expect(component.debug()).toMatchSnapshot();                          
     const wrapper = component.find(".App");
     expect(wrapper.length).toBe(1);
   });
