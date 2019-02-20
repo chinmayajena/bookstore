df7c9c00e1f03adbc1827ea95987285327a15fa1
Chinmaya Jena <mail2chinmaya.jena@gmail.com>
2019-02-20 17:48:28 +0530

    removed unnecessary files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import books from "./BookReducer.js";
 import visibilityFilter from "./FilterReducer.js";
-import sortBy from "./SortReducer.js";
 import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
   books,
   visibilityFilter,
-  sortBy,
   form: formReducer
 });
