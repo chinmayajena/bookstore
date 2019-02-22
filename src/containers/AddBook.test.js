13cb77f61eb7d15de256509334b4bc8dad8559ec
Chinmaya Jena <mail2chinmaya.jena@gmail.com>
2019-02-22 09:11:05 +0530

    Test cases for Add book form

--- src/containers/AddBook.test.js
+++ src/containers/AddBook.test.js
@@ -1,22 +1,46 @@
 import React from "react";
-import { shallow } from "enzyme";
-import { AddBook } from "./AddBook.js";
+import { shallow, mount } from "enzyme";
+import ConnectedAddBook, { AddBook } from "./AddBook.js";
 import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
 describe("<AddBook />", () => {
-  const initialState = {};
-  const mockStore = configureStore();
-  const mockSubmitfn = jest.fn();
-  let store, component;
-
+  let component;
+  const addBookfn = jest.fn();
+  let appHistory = [];
   beforeEach(() => {
-    store = mockStore(initialState);
-    component = shallow(<AddBook addBook={mockSubmitfn} store={store} />);
+    component = shallow(<AddBook addBook={addBookfn} history={appHistory} />);
   });
 
   it("renders the component", () => {
     expect(component.debug()).toMatchSnapshot();
     const wrapper = component.find("ErrorBoundary");
     expect(wrapper.length).toBe(1);
+  });
+
+  it("submits the form", () => {
+    component.find("ReduxForm").simulate("submit", { preventDefault() {} });
+    expect(addBookfn.mock.calls.length).toBe(1);
+  });
+});
+
+describe("<ConnectedAddBook />", () => {
+  const initialState = {};
+  const mockStore = configureStore();
+  const addBookfn = jest.fn();
+  let store, container;
+
+  beforeEach(() => {
+    store = mockStore(initialState);
+    container = mount(
+      <Provider store={store}>
+        <ConnectedAddBook addBook={addBookfn} />
+      </Provider>
+    );
+  });
+
+  it("+++ render the connected component", () => {
+    expect(container).toMatchSnapshot();
+    expect(container.length).toEqual(1);
   });
 });
